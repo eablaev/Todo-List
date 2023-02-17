87ab6f2920b192f64ef6bb5b7b6d6151f3e68f33
shmerv <eaudioworks@gmail.com>
2023-02-16 22:22:37 -0800

    Added some css styling and show clas to the new todo window

--- src/displayTodos.js
+++ src/displayTodos.js
@@ -1,18 +1,39 @@
 export default function display (array) {
-    console.log('inside display func');
-    const contentDiv = document.getElementById('contentDiv')
-    contentDiv.textContent = '';
+    
+    const todoContainer = document.getElementById('todoContainer')
+    todoContainer.textContent = '';
+
     array.forEach(element => {
         const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todoDiv');
         
-        const todoTitle = document.createElement('h3');
+        const todoTitle = document.createElement('h5');
+        todoTitle.classList.add('todoTitle');
         todoTitle.innerHTML = element.title;
 
-        todoDiv.appendChild(todoTitle);
+        const todoDescription  = document.createElement('h6');
+        todoDescription.classList.add('todoDescription');
+        todoDescription.innerHTML = element.description;
+
+        const dueDate = document.createElement('h6');
+        dueDate.classList.add('dueDate');
+        dueDate.innerHTML = element.dueDate;
+
+        const todoDivLeft = document.createElement('div');
+        todoDivLeft.classList.add('todoDivLeft');
+        const todoDivRight = document.createElement('div');
+        todoDivRight.classList.add('todoDivRight');
+
+        todoDiv.appendChild(todoDivLeft);
+        todoDiv.appendChild(todoDivRight);
 
 
+        todoDivLeft.appendChild(todoTitle);
+        todoDivRight.appendChild(todoDescription);
+        todoDivRight.appendChild(dueDate);
 
-        contentDiv.appendChild(todoDiv)
+
+        todoContainer.appendChild(todoDiv);
 
     });
 }
--- src/index.js
+++ src/index.js
@@ -3,19 +3,43 @@ import TodoItem from './todoMaker';
 import display from './displayTodos';
 import './style.css';
 
-const todoArr = [];
+const todoArr = [
+    {
+        title: 'Hello',
+        description:"This is short description",
+        dueDate:'02/02/02',
+        priority:'high'
+    }
+];
+display(todoArr);
+
+
 
 const form = document.getElementById('form');
+const formContainer = document.getElementById('formContainer')
+const addNew = document.getElementById('addNewTodo');
+addNew.addEventListener('click', function(){
+    console.log('h')
+    formContainer.classList.add('show');
+    console.log(formContainer.classList)
+})
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    const title = document.getElementById('title').value
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const dueDate = document.getElementById('dueDate').value;
     
-    const todo = new TodoItem(title);
+    
+    const todo = new TodoItem(title,description,dueDate,priority);
     todoArr.push(todo);
 
     display(todoArr)
+
+    console.log(todoArr);
+
+    formContainer.classList.remove('show')
 
 });
 
